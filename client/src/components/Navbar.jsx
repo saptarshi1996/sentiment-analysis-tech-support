b1978bdfa820d1ee8730c19af925337255f765bc
saptarshi1996 <saptarshisg96@gmail.com>
2025-01-07 06:49:16 +0530

    format imports

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ import {
   Typography,
 } from '@mui/material';
 
-import { useNavigate } from 'react-router-dom';
+import {
+  useNavigate
+} from 'react-router-dom';
 
 export default function Navbar() {
   const navigate = useNavigate();
